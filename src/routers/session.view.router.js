765119bc0ef96cec58ec708a8f3eed541f7e69de
Vic <victorialau.dg@gmail.com>
2023-07-31 12:40:58 -0300

    agregado de ruta test

--- src/routers/session.view.router.js
+++ src/routers/session.view.router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { privateRoutes, publicRoutes } from '../middlewares/auth.middleware.js'
 import UserDTO from '../dto/user.dto..js'
+import logger from './../logger.js'
 
 const router = Router()
 
@@ -15,6 +16,16 @@ router.get('/', privateRoutes, (req, res) => {
 router.get('/profile', publicRoutes, (req, res) => {
     const userDTO = new UserDTO(req.session.user)
     res.render('sessions/profile', userDTO)
+})
+
+router.get('/loggerTest', (req, res) => {
+    logger.debug('Mensaje de debug');
+    logger.http('Mensaje de petición HTTP');
+    logger.info('Mnsaje de información');
+    logger.warning('Mensaje de advertencia');
+    logger.error('Mensaje de error');
+    logger.fatal('Mensaje de error fatal');
+    res.json({ status: 'success', message: 'Pruebas ejecutadas exitosamente' })
 })
 
 export default router
